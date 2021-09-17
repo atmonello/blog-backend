ce737ba034e9bbbfaeeca1f246644885bdc1a8f0
Andre Monello <atmonello@outlook.com>
2021-09-16 21:26:40 -0300

    (feat) bump

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env("URL", "http://localhost:1337"),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'e69bb6b9e82776fbbd573a5fc0cde945'),
